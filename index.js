5fc08beb7569ba1de79cad54d16bf38e77288c94
Henrique Mauler <henriquemauler@gmail.com>
2021-06-10 18:56:27 -0300

    added node.js back-end at the project (incomplete)

---
+++ index.js
@@ -0,0 +1,93 @@
+import http from 'http';
+import fs from 'fs'
+const port = 3000
+const host = 'localhost'
+
+
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log("Carregando as páginas")
+const pages_directory = join(__dirname,"pages") 
+const pages = {
+    index : fs.readFileSync(join(pages_directory,"index.html"),{encoding : 'utf-8'}),
+}
+
+
+const styles = [
+    fs.readFileSync(join(pages_directory,"dashboard.css"),{encoding : 'utf-8'}),
+    fs.readFileSync(join(pages_directory,"mystyle.css"),{encoding : 'utf-8'}),
+    fs.readFileSync(join(pages_directory,"registration.css"),{encoding : 'utf-8'}),
+]
+console.log("Páginas carregadas");
+
+
+
+const server = http.createServer((request,response) => {
+
+    response.writeHead(200, {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods' : 'OPTIONS,POST,GET',
+    'Content-Type' : 'application/html'
+})
+
+
+        console.log("Acessando URL: " + request.url)
+
+        switch(request.url) {
+            case "/":
+                response.writeHead(200)
+                
+                response.end("Entrando na rota principal")
+                break
+            case "/home" :
+                console.log("TIPAGEM: " + typeof pages.index)
+                
+                response.end(pages.index)
+                break
+
+
+            case '/pages/mystyle.css':
+                response.setHeader("Content-Type","application/css")
+                
+                response.end(styles[1]);
+                console.log("Pegando meu estilo");
+                break;
+            case '/pages/dashboard.css':
+                response.setHeader("Content-Type","application/css")
+                
+                response.end(styles[0]);
+                
+                break;
+            case '/pages/registration.css':
+                response.setHeader("Content-Type","application/css")
+                response.writeHead(200)
+                response.end(styles[2]);
+                break;
+                
+            default:
+                response.writeHead(404)
+                response.end("Algo de errado não está certo, a página não foi encontrada")
+                break
+        }
+
+
+    // console.log("Olá mundo " + pages_directory);
+    // onRequestedInformation(req,res)
+})
+
+
+server.listen(port,host, () => {
+    console.log("Servidor está rodando na em http://" + host + ":" + port);
+    console.log("Olá mundo " + pages_directory);
+})
+
+function onRequestedInformation(request, response) {
+
+}
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+- [] Registro de usuário 
+- [] Carregar as coletâneas 
+- [] Alugar um livro
+- [] Inserir um livro
+
+
+- [] Alterar o nome do arquivo registration para login
+- [] Alterar o nome do arquivo Dashboard para add user book
+- [] Alterar o nome do arquivo index para add user dashboard
