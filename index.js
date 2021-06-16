701c5f72e086729a1e83fbba6498087dd60ec577
rickymal <henriquemauler@gmail.com>
2021-06-16 15:44:41 -0300

    solved problem with sqlite database

--- index.js
+++ index.js
@@ -38,7 +38,10 @@ const styles = [
 /* Database space */
 
 
-const sequelize = new Sequelize("sqlite::memory:");
+const sequelize = new Sequelize({
+    dialect : 'sqlite',
+    storage : 'database.sqlite'
+});
 
 class User extends Model {
     
@@ -54,23 +57,60 @@ var dt = DataTypes
 User.init({
     email : dt.STRING,
     password : dt.STRING,
-},{sequelize})
+},{sequelize, modelName : 'user'})
 
 Book.init({
     title : dt.STRING,
     description : dt.TEXT,
-}, {sequelize})
+}, {sequelize, modelName : 'book'})
 
 
 User.hasMany(Book)
-Book.belongsTo(User);
+Book.belongsTo(User, {
+    foreignKey : {
+        allowNull : true,
+    },
+    constraints : true,
+});
+
+// Team.hasMany(Mentoring);
+// Mentoring.belongsTo(Team);
+
 
 async function initializeSequelize() {
     await sequelize.sync({force : true})
 }
 
-initializeSequelize();
+await initializeSequelize();
 
+
+
+const rique_user = await User.create({
+    email : "henriquemauler@gmail.com",
+    password : "123456789",
+})
+
+
+const another_user = await User.create({
+    email : "anotheremail@tylok.com",
+    password : "123456789",
+})
+
+
+const book1 = await Book.create({
+    title : "Knowing yourself",
+    description : "This is a book that make you know about yourself better",
+})
+
+const book2 = await Book.create({
+    title : "The narnia Chronics",
+    description : "This is a book that will teach you about a imaginary space",
+})
+
+
+
+book1.setUser(rique_user)
+book2.setUser(another_user)
 
 
 
@@ -118,7 +158,6 @@ const server = http.createServer((request,response) => {
                 break;
             case '/dashboard.css':
                 response.setHeader("Content-Type","text/css")
-                
                 response.writeHead(200)
                 response.end(styles[0]);
                 
@@ -129,12 +168,29 @@ const server = http.createServer((request,response) => {
                 response.end(styles[2]);
                 break;
 
-            case "/api/send_notification":
-                
+            case "/api/make_login":
+                let body = "";
+                console.log("Verificando a request: " + request.method)
+                console.log("Verificando a request: " + request.httpVersion)
+                request.on('data',chunk => {
+                    console.log("Chamando uma chunk")
+                    body += chunk
+                })
+
+                request.on('end',() => {
+                    response.setHeader("Content-type","text/plain")
+                    response.writeHead(200)
+                    response.end("O conteúdo enviado é: " + body);
+                    
+                })
+
+
+                request.on('error',err => console.log("Algo de errado no método POST não está certo: " + err))
                 break;
                 
             default:
                 response.writeHead(404)
+                
                 response.end("Algo de errado não está certo, a página não foi encontrada","utf-8")
                 break;
         }
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 - [] Registro de usuário 
+    - [] Resolver problema com Sequelize
+    - [] 
 - [] Carregar as coletâneas 
 - [] Alugar um livro
 - [] Inserir um livro
@@ -8,4 +10,5 @@
 - [] Alterar o nome do arquivo Dashboard para add user book
 - [] Alterar o nome do arquivo index para add user dashboard
 - [] Adicionar caminho ao path
+- [] Colocar uma verificação no método set dinâmico do sequelize se o objeto inserido é uma promise, pois isso as vezes é passado despercebido pelo desenvolvedor.
 
